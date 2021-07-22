254057bafe2debe48a763a0111b1140ea98a36ce
Maurice Chevez <mauricechevez@gmail.com>
2021-07-22 18:04:28 -0400

    Total per person added

--- js/index.js
+++ js/index.js
@@ -13,32 +13,41 @@ let totalAmountDisplay = document.getElementById('total-amount');
 
 //variables
 let percentageAmount = 0;
-let totalAmount = 0;
+let totalTipAmount = 0;
+let totalBillAmount = 0;
 
 // ****** Pseudo Code for calculation
 // amountInput is entered by the person
 // Button is clicked. Depending on button pressed, the amount returned back should be different since different percentage numbers.
 
 const buttonPressedValue = (e)=>{
-    if(amountInput.value === ''){
-        // the input box is empty. Validate what to do if it is empty.
-        amountInput.classList.add('calculation-amount-input--error')
-        inputErrorMessage.classList.add('calculation-amount__err-msg--active')
-        console.log('Please fill this out')
-    } else {
         const percentageString = e.textContent
         const percentageNumber = parseInt(percentageString.replace('%',''))
         percentageAmount = percentageNumber;
-    } return percentageAmount; 
-}
+        return percentageAmount; 
+    } 
+
 
 const calculatePct = (num, pct)=>{
     if(peopleAmount.value === '0'){
         console.log('This cannot be 0')
     } else {
-    totalAmount = (parseFloat(num/100)*pct)/parseInt(peopleAmount.value);
+    totalTipAmount = (parseFloat(num/100)*pct)/parseInt(peopleAmount.value);
+    totalBillAmount = (totalTipAmount + parseFloat(amountInput.value)) / peopleAmount.value
     }
-   return totalAmount;
+   return totalTipAmount;
+}
+
+function addActive(btn){
+    btn.classList.add('calculation__btn--active')
+}
+
+//https://clubmate.fi/remove-a-class-name-from-multiple-elements-with-pure-javascript
+function removeActives(){
+    const els = document.getElementsByClassName('calculation__btn--active')
+    while (els[0]){
+    els[0].classList.remove('calculation__btn--active')
+    }
 }
 
 /* ########### Event Listeners ########### */
@@ -48,39 +57,71 @@ btn5Pct.addEventListener('click',()=>{
         // the input box is empty. Validate what to do if it is empty.
         amountInput.classList.add('calculation-amount-input--error')
         inputErrorMessage.classList.add('calculation-amount__err-msg--active')
-        console.log('Please fill this out')
     } else {
         buttonPressedValue(btn5Pct)
         calculatePct(amountInput.value,percentageAmount)
-        // console.log(totalAmount.toFixed(2))
-        tipAmount.textContent = totalAmount.toFixed(2);
+        removeActives()
+        addActive(btn5Pct)
+        tipAmount.textContent = totalTipAmount.toFixed(2);
+        totalAmountDisplay.textContent = totalBillAmount.toFixed(2)
     }
 })
 
 btn10Pct.addEventListener('click',()=>{
+    if(amountInput.value === ''){
+        amountInput.classList.add('calculation-amount-input--error')
+        inputErrorMessage.classList.add('calculation-amount__err-msg--active')
+    } else {
     buttonPressedValue(btn10Pct)
     calculatePct(amountInput.value,percentageAmount)
-    // console.log(totalAmount.toFixed(2))
-    tipAmount.textContent = totalAmount.toFixed(2);
-
+    removeActives()
+    addActive(btn10Pct)
+    // console.log(totalTipAmount.toFixed(2))
+    tipAmount.textContent = totalTipAmount.toFixed(2);
+    totalAmountDisplay.textContent = totalBillAmount.toFixed(2)
+    }
 })
 btn15Pct.addEventListener('click',()=>{
+    if(amountInput.value === ''){
+        amountInput.classList.add('calculation-amount-input--error')
+        inputErrorMessage.classList.add('calculation-amount__err-msg--active')
+    } else {
     buttonPressedValue(btn15Pct)
     calculatePct(amountInput.value,percentageAmount)
-    // console.log(totalAmount.toFixed(2))
-    tipAmount.textContent = totalAmount.toFixed(2);
+    removeActives()
+    addActive(btn15Pct)
+    // console.log(totalTipAmount.toFixed(2))
+    tipAmount.textContent = totalTipAmount.toFixed(2);
+    totalAmountDisplay.textContent = totalBillAmount.toFixed(2)
+    }
 })
 btn25Pct.addEventListener('click',()=>{
+    if(amountInput.value === ''){
+        amountInput.classList.add('calculation-amount-input--error')
+        inputErrorMessage.classList.add('calculation-amount__err-msg--active')
+    } else {
     buttonPressedValue(btn25Pct)
     calculatePct(amountInput.value,percentageAmount)
-    // console.log(totalAmount.toFixed(2))
-    tipAmount.textContent = totalAmount.toFixed(2);
+    removeActives()
+    addActive(btn25Pct)
+    // console.log(totalTipAmount.toFixed(2))
+    tipAmount.textContent = totalTipAmount.toFixed(2);
+    totalAmountDisplay.textContent = totalBillAmount.toFixed(2)
+    }
 })
 btn50Pct.addEventListener('click',()=>{
+    if(amountInput.value === ''){
+        amountInput.classList.add('calculation-amount-input--error')
+        inputErrorMessage.classList.add('calculation-amount__err-msg--active')
+    } else {
     buttonPressedValue(btn50Pct)
     calculatePct(amountInput.value,percentageAmount)
-    // console.log(totalAmount.toFixed(2))
-    tipAmount.textContent = totalAmount.toFixed(2);
+    removeActives()
+    addActive(btn50Pct)
+    // console.log(totalTipAmount.toFixed(2))
+    tipAmount.textContent = totalTipAmount.toFixed(2);
+    totalAmountDisplay.textContent = totalBillAmount.toFixed(2)
+    }
 })
 
 btnCustom.addEventListener('click',()=>{
